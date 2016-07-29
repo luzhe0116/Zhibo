fc887ae6958c8adbb6e9fff4e13152646502d17a
luzhe <luzhe0516@163.com>
2016-07-29 13:51:42 +0800

    Initial Commit

---
+++ Broadcast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Broadcast
+//
+//  Created by 卢晨笑 on 16/7/29.
+//  Copyright © 2016年 luzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
